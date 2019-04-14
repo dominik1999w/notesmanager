081edcc2763d6d6b16257ba5a3bf8dee122a45fe
Jędrzej Hodor <jedrzej.hodor@gmail.com>
2019-04-14 12:36:48 +0000

    Adding structure + back button

--- src/sample/Launcher.java
+++ src/sample/Launcher.java
@@ -1,24 +1,22 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-public class Launcher extends Application{
+
+public class Launcher extends Application {
+
     private static final String MAIN_FXML = "sample.fxml";
     private static final String SCENE_ONE_FXML = "CategoryClass.fxml";
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
-        Pane myPane = loader.load();
-        Controller controller = loader.getController();
-        controller.setPreviousStage(primaryStage);
-        Scene scene = new Scene(myPane);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        //Make first primary controller, set its name. It will create static field for
+        //Controller named StageMaster, who will receive primaryStage and will store & use it for every scene.
+        Controller controllerPrimary = new ControllerPrimary("sample.fxml", primaryStage);
+        //Static stageMaster is loading first scene using controllerPrimary.
+        Controller.stageMaster.loadNewScene(controllerPrimary);
     }
-    public static void main(String[] args) {
-        launch(args);
-    }
+
+    public static void main(String[] args) { launch(args); }
+
 }
