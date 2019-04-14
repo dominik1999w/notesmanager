91e5550e1021ffe584109ea5ee599056917bcc9e
Jędrzej Hodor <jedrzej.hodor@gmail.com>
2019-04-14 12:38:03 +0000

    Adding StageMaster

--- src/sample/StageMaster.java
+++ src/sample/StageMaster.java
@@ -1,7 +1,40 @@
 package sample;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class StageMaster {
 
+    private Controller currentController;
+    private Stage stage;
+
+    public StageMaster(Stage stage) {
+        this.stage = stage;
+    }
+
+    //While pressing buttons those methods should be applied:
+
+    public void loadNewScene(Controller controller) throws IOException {
+        //Controller should be created while pressing button.
+        this.currentController = controller;
+        loadScene(currentController.getName());
+    }
+
+    public void loadPreviousScene() throws IOException {
+        loadNewScene(currentController.getPreviousController());
+    }
+
+    private void loadScene(String name) throws IOException {
+        FXMLLoader loader = new FXMLLoader(currentController.getClass().getResource(name));
+        loader.setController(currentController);
+        Pane myPane = loader.load();
+        this.stage.setScene(new Scene(myPane));
+        stage.show();
+    }
 
 
 }
